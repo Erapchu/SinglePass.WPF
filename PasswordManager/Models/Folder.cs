e6d7aa6122bc798c1702858d5af3f9022797a934
Andrey Nasonov <andrey.nasonov@actonica.ru>
2021-02-14 18:01:13 +0700

    Added folder and VM

---
+++ PasswordManager/Models/Folder.cs
@@ -0,0 +1,16 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Models
+{
+    public class Folder
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; set; }
+        public PackIconKind Icon { get; set; }
+    }
+}
---
+++ PasswordManager/ViewModels/FolderItemViewModel.cs
@@ -0,0 +1,47 @@
+﻿using MaterialDesignThemes.Wpf;
+using PasswordManager.Helpers;
+using PasswordManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.ViewModels
+{
+    public class FolderItemViewModel : ViewModelBase
+    {
+        public Folder Model { get; }
+
+        public string Name
+        {
+            get => Model.Name;
+            set
+            {
+                if (Model.Name == value)
+                    return;
+
+                Model.Name = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public PackIconKind IconKind
+        {
+            get => Model.Icon;
+            set
+            {
+                if (Model.Icon == value)
+                    return;
+
+                Model.Icon = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public FolderItemViewModel(Folder folder)
+        {
+            Model = folder ?? throw new ArgumentNullException(nameof(folder));
+        }
+    }
+}

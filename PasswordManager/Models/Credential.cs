d15840c1e96acf2d5b3eb64f1cde9ec81ed1873b
Andrey Nasonov <andrey.nasonov@actonica.ru>
2021-02-22 00:44:10 +0700

    Added credentials model and business logic

---
+++ PasswordManager/Models/Credential.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Models
+{
+    public class Credential
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Other { get; set; }
+    }
+}
---
+++ PasswordManager/ViewModels/CredentialViewModel.cs
@@ -0,0 +1,22 @@
+﻿using PasswordManager.Helpers;
+using PasswordManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.ViewModels
+{
+    public class CredentialViewModel : ViewModelBase
+    {
+        public Credential Model { get; }
+
+        public string Name => Model.Name;
+
+        public CredentialViewModel(Credential credential)
+        {
+            Model = credential;
+        }
+    }
+}

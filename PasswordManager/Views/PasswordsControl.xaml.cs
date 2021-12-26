bc060b427f68daa3d636553d014eac0eb4e73387
Andrey Nasonov <andrey.nasonov@actonica.ru>
2021-12-26 18:33:42 +0700

    Renamed main content to passwords control

---
+++ PasswordManager/Views/PasswordsControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PasswordManager.Views
+{
+    /// <summary>
+    /// Interaction logic for PasswordsControl.xaml
+    /// </summary>
+    public partial class PasswordsControl : UserControl
+    {
+        public PasswordsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

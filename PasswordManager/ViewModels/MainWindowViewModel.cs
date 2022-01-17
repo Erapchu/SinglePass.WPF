14d4ee5415dc1d4f4a47ad6ea8bccae6b4d3d64b
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-01-17 21:13:10 +0700

    Using new navigation item in main window

--- PasswordManager/ViewModels/MainWindowViewModel.cs
+++ PasswordManager/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
+using NLog;
+using PasswordManager.Collections;
 using PasswordManager.Services;
+using PasswordManager.Views;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace PasswordManager.ViewModels
@@ -20,20 +25,62 @@ namespace PasswordManager.ViewModels
         #endregion
 
         private readonly SettingsService _settingsService;
+        private readonly ILogger _logger;
 
         public PasswordsViewModel PasswordsViewModel { get; }
         public SettingsViewModel SettingsViewModel { get; }
+        public ObservableCollectionDelayed<NavigationItemViewModel> NavigationItems { get; }
+
+        private NavigationItemViewModel _selectedNavigationItem;
+        public NavigationItemViewModel SelectedNavigationItem
+        {
+            get => _selectedNavigationItem;
+            set
+            {
+                if (_selectedNavigationItem == value)
+                    return;
+
+                // Prevent unselected items
+                if (value is null)
+                    return;
+
+                _selectedNavigationItem = value;
+                OnPropertyChanged();
+            }
+        }
 
         private MainWindowViewModel() { }
 
         public MainWindowViewModel(
             PasswordsViewModel passwordsViewModel,
             SettingsService settingsService,
-            SettingsViewModel settingsViewModel)
+            SettingsViewModel settingsViewModel,
+            ILogger logger)
         {
             PasswordsViewModel = passwordsViewModel;
             SettingsViewModel = settingsViewModel;
             _settingsService = settingsService;
+            _logger = logger;
+
+            NavigationItems = new ObservableCollectionDelayed<NavigationItemViewModel>(GenerateSettingsItem());
+            SelectedNavigationItem = NavigationItems.First();
+        }
+
+        private List<NavigationItemViewModel> GenerateSettingsItem()
+        {
+            return new List<NavigationItemViewModel>()
+            {
+                new NavigationItemViewModel(
+                    "Credentials",
+                    MaterialDesignThemes.Wpf.PackIconKind.Password,
+                    () => new PasswordsControl() { DataContext = PasswordsViewModel },
+                    _logger),
+                new NavigationItemViewModel(
+                    "Settings",
+                    MaterialDesignThemes.Wpf.PackIconKind.Settings,
+                    () => new SettingsControl() { DataContext = SettingsViewModel },
+                    _logger),
+            };
         }
 
         private async Task LoadingAsync()
---
+++ PasswordManager/ViewModels/NavigationItemViewModel.cs
@@ -0,0 +1,61 @@
+﻿using MaterialDesignThemes.Wpf;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using NLog;
+using System;
+using System.Windows.Controls;
+
+namespace PasswordManager.ViewModels
+{
+    public class NavigationItemViewModel : ObservableRecipient
+    {
+        private readonly ILogger _logger;
+        private readonly Lazy<Control> _lazyContent;
+
+        /// <summary>
+        /// Name of settings item.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Visible content.
+        /// </summary>
+        public Control Content
+        {
+            get
+            {
+                try
+                {
+                    return _lazyContent.Value;
+                }
+                catch (Exception ex)
+                {
+                    _logger.Error(ex);
+                    return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Icon kind.
+        /// </summary>
+        public PackIconKind IconKind { get; }
+
+        /// <summary>
+        /// Constructs settings item view model with specified name, content and icon.
+        /// </summary>
+        /// <param name="name">Name of settings item.</param>
+        /// <param name="createContent">Function to create content.</param>
+        /// <param name="packIconKind">Icon kind on the left side.</param>
+        public NavigationItemViewModel(
+            string name,
+            PackIconKind packIconKind,
+            Func<Control> createContent,
+            ILogger logger)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            _lazyContent = new Lazy<Control>(createContent);
+            IconKind = packIconKind;
+            _logger = logger;
+        }
+    }
+}

a28986489149c3144d7d61c38eb4f82f5c107679
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-01-24 10:27:19 +0700

    Added using for encryptor
    dispose required for that object

--- PasswordManager/Helpers/AesCryptographyHelper.cs
+++ PasswordManager/Helpers/AesCryptographyHelper.cs
@@ -31,17 +31,18 @@ namespace PasswordManager.Helpers
                 aesAlg.IV = IV;
 
                 // Create an encryptor to perform the stream transform.
-                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-                // Create the streams used for encryption.
-                using MemoryStream msEncrypt = new MemoryStream();
-                using CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
-                using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                using (var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV))
                 {
-                    //Write all data to the stream.
-                    swEncrypt.Write(plainText);
-                }
-                encrypted = msEncrypt.ToArray();
+                    // Create the streams used for encryption.
+                    using var msEncrypt = new MemoryStream();
+                    using var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+                    using (var swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        // Write all data to the stream.
+                        swEncrypt.Write(plainText);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                } 
             }
 
             // Return the encrypted bytes from the memory stream.
@@ -70,15 +71,16 @@ namespace PasswordManager.Helpers
                 aesAlg.IV = IV;
 
                 // Create a decryptor to perform the stream transform.
-                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-
-                // Create the streams used for decryption.
-                using MemoryStream msDecrypt = new MemoryStream(cipherText);
-                using CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
-                using StreamReader srDecrypt = new StreamReader(csDecrypt);
-                // Read the decrypted bytes from the decrypting stream
-                // and place them in a string.
-                plaintext = srDecrypt.ReadToEnd();
+                using (var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV))
+                {
+                    // Create the streams used for decryption.
+                    using var msDecrypt = new MemoryStream(cipherText);
+                    using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+                    using var srDecrypt = new StreamReader(csDecrypt);
+                    // Read the decrypted bytes from the decrypting stream
+                    // and place them in a string.
+                    plaintext = srDecrypt.ReadToEnd();
+                }
             }
 
             return plaintext;

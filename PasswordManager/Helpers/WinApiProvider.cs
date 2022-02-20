81505b2f4d5eaf799e32463fd7f4d0a0a7d4b19b
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-02-20 20:15:19 +0700

    Start replacing icon check converter

---
+++ PasswordManager/Helpers/WinApiProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PasswordManager.Helpers
+{
+    internal static class WinApiProvider
+    {
+        public const int IDI_APPLICATION = 0x7F00;
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern IntPtr LoadImage(IntPtr hinst, IntPtr lpszName, uint uType, int cxDesired, int cyDesired, uint fuLoad);
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}

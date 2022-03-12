afa89b1449b4c9a3f88a5e34efd2eb9579825bbc
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-03-12 21:48:23 +0700

    Implemented file update

--- PasswordManager/Clouds/Services/GoogleDriveCloudService.cs
+++ PasswordManager/Clouds/Services/GoogleDriveCloudService.cs
@@ -1,11 +1,13 @@
 ﻿using PasswordManager.Authorization.Brokers;
 using PasswordManager.Authorization.Interfaces;
 using PasswordManager.Clouds.Interfaces;
+using PasswordManager.Clouds.Models;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Net.Mime;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -42,12 +44,30 @@ namespace PasswordManager.Clouds.Services
             var client = _httpClientFactory.CreateClient();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
+            // Check and search file
+            using var filesResponse = await client.GetAsync("https://www.googleapis.com/drive/v3/files?q=trashed%3Dfalse", cancellationToken);
+            using var content = filesResponse.Content;
+            var jsonResponse = await content.ReadAsStringAsync(cancellationToken);
+            var fileList = JsonSerializer.Deserialize<GoogleDriveFileList>(jsonResponse);
+            var targetFile = fileList.Files.Find(f => f.Name == fileName);
+
+            // Prepare request
             var metaContent = JsonContent.Create(new { name = fileName });
             var streamContent = new StreamContent(stream);
             var multipart = new MultipartContent { metaContent, streamContent };
             streamContent.Headers.ContentType = new MediaTypeHeaderValue(MediaTypeNames.Application.Octet);
             streamContent.Headers.ContentLength = stream.Length;
-            var result = await client.PostAsync("https://www.googleapis.com/upload/drive/v3/files?uploadtype=multipart", multipart, cancellationToken);
+
+            if (targetFile != null)
+            {
+                // Update
+                var r = await client.PatchAsync($"https://www.googleapis.com/upload/drive/v3/files/{targetFile.Id}?uploadtype=multipart", multipart, cancellationToken);
+            }
+            else
+            {
+                // Create
+                var r = await client.PostAsync("https://www.googleapis.com/upload/drive/v3/files?uploadtype=multipart", multipart, cancellationToken);
+            }
         }
     }
 }

b9133fc2dcf4ec188e567db5ff9da33cfb3a4890
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-03-30 20:51:01 +0700

    Don't set headers on http client
    Use sendasync for each call

--- PasswordManager/Clouds/Services/GoogleDriveCloudService.cs
+++ PasswordManager/Clouds/Services/GoogleDriveCloudService.cs
@@ -31,29 +31,40 @@ namespace PasswordManager.Clouds.Services
         {
             if (AuthorizationBroker.TokenHolder.Token.RefreshRequired)
             {
-                await AuthorizationBroker.RefreshAccessToken(cancellationToken);
+                await AuthorizationBroker.RefreshAccessToken(cancellationToken).ConfigureAwait(false);
             }
         }
 
-        private HttpClient GetHttpClient()
+        private HttpRequestMessage GetAuthHttpRequestMessage()
         {
-            var accessToken = AuthorizationBroker.TokenHolder.Token.AccessToken;
+            var requestMessage = new HttpRequestMessage();
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", AuthorizationBroker.TokenHolder.Token.AccessToken);
+            return requestMessage;
+        }
 
+        private async Task<GoogleDriveFileList> GetGoogleDriveFileList(CancellationToken cancellationToken)
+        {
+            await RefreshAccessTokenIfRequired(cancellationToken).ConfigureAwait(false);
             var client = _httpClientFactory.CreateClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            return client;
+
+            using var getFilesRM = GetAuthHttpRequestMessage();
+            getFilesRM.Method = HttpMethod.Get;
+            getFilesRM.RequestUri = new System.Uri("https://www.googleapis.com/drive/v3/files?q=trashed%3Dfalse");
+            using var filesResponse = await client.SendAsync(getFilesRM, cancellationToken).ConfigureAwait(false);
+            using var content = filesResponse.Content;
+            var jsonResponse = await content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+            var fileList = JsonSerializer.Deserialize<GoogleDriveFileList>(jsonResponse);
+
+            return fileList;
         }
 
         public async Task Upload(Stream stream, string fileName, CancellationToken cancellationToken)
         {
-            await RefreshAccessTokenIfRequired(cancellationToken);
-            var client = GetHttpClient();
+            await RefreshAccessTokenIfRequired(cancellationToken).ConfigureAwait(false);
+            var client = _httpClientFactory.CreateClient();
 
             // Check and search file
-            using var filesResponse = await client.GetAsync("https://www.googleapis.com/drive/v3/files?q=trashed%3Dfalse", cancellationToken);
-            using var content = filesResponse.Content;
-            var jsonResponse = await content.ReadAsStringAsync(cancellationToken);
-            var fileList = JsonSerializer.Deserialize<GoogleDriveFileList>(jsonResponse);
+            var fileList = await GetGoogleDriveFileList(cancellationToken).ConfigureAwait(false);
             var targetFile = fileList.Files.Find(f => f.Name == fileName);
 
             // Prepare request
@@ -66,42 +77,54 @@ namespace PasswordManager.Clouds.Services
             if (targetFile != null)
             {
                 // Update
-                var r = await client.PatchAsync($"https://www.googleapis.com/upload/drive/v3/files/{targetFile.Id}?uploadtype=multipart", multipart, cancellationToken);
+                using var updateFileRM = GetAuthHttpRequestMessage();
+                updateFileRM.Method = HttpMethod.Patch;
+                updateFileRM.RequestUri = new System.Uri($"https://www.googleapis.com/upload/drive/v3/files/{targetFile.Id}?uploadtype=multipart");
+                updateFileRM.Content = multipart;
+                var r = await client.SendAsync(updateFileRM, cancellationToken).ConfigureAwait(false);
             }
             else
             {
                 // Create
-                var r = await client.PostAsync("https://www.googleapis.com/upload/drive/v3/files?uploadtype=multipart", multipart, cancellationToken);
+                using var createFileRM = GetAuthHttpRequestMessage();
+                createFileRM.Method = HttpMethod.Post;
+                createFileRM.RequestUri = new System.Uri("https://www.googleapis.com/upload/drive/v3/files?uploadtype=multipart");
+                createFileRM.Content = multipart;
+                var r = await client.SendAsync(createFileRM, cancellationToken).ConfigureAwait(false);
             }
         }
 
         public async Task<Stream> Download(string fileName, CancellationToken cancellationToken)
         {
-            await RefreshAccessTokenIfRequired(cancellationToken);
-            var client = GetHttpClient();
+            await RefreshAccessTokenIfRequired(cancellationToken).ConfigureAwait(false);
 
-            using var filesResponse = await client.GetAsync("https://www.googleapis.com/drive/v3/files?q=trashed%3Dfalse", cancellationToken);
-            using var filesContent = filesResponse.Content;
-            var jsonResponse = await filesContent.ReadAsStringAsync(cancellationToken);
-            var fileList = JsonSerializer.Deserialize<GoogleDriveFileList>(jsonResponse);
+            // Check and search file
+            var fileList = await GetGoogleDriveFileList(cancellationToken).ConfigureAwait(false);
             var targetFile = fileList.Files.Find(f => f.Name == fileName);
-
             if (targetFile is null)
                 return null;
 
-            var fileResponse = await client.GetAsync($"https://www.googleapis.com/drive/v3/files/{targetFile.Id}?alt=media", cancellationToken);
+            var client = _httpClientFactory.CreateClient();
+            using var getFileStreamRM = GetAuthHttpRequestMessage();
+            getFileStreamRM.Method = HttpMethod.Get;
+            getFileStreamRM.RequestUri = new System.Uri($"https://www.googleapis.com/drive/v3/files/{targetFile.Id}?alt=media");
+            var fileResponse = await client.SendAsync(getFileStreamRM, cancellationToken).ConfigureAwait(false);
             var fileContent = fileResponse.Content;
-            return await fileContent.ReadAsStreamAsync(cancellationToken);
+            return await fileContent.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
         }
 
         public async Task<BaseUserInfo> GetUserInfo(CancellationToken cancellationToken)
         {
-            await RefreshAccessTokenIfRequired(cancellationToken);
-            var client = GetHttpClient();
+            await RefreshAccessTokenIfRequired(cancellationToken).ConfigureAwait(false);
+            var client = _httpClientFactory.CreateClient();
 
-            using var userInfoResponse = await client.GetAsync("https://www.googleapis.com/oauth2/v3/userinfo", cancellationToken);
+            using var requestMessage = GetAuthHttpRequestMessage();
+
+            requestMessage.Method = HttpMethod.Get;
+            requestMessage.RequestUri = new System.Uri("https://www.googleapis.com/oauth2/v3/userinfo");
+            using var userInfoResponse = await client.SendAsync(requestMessage, cancellationToken).ConfigureAwait(false);
             using var content = userInfoResponse.Content;
-            var jsonResponse = await content.ReadAsStringAsync(cancellationToken);
+            var jsonResponse = await content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
             var userInfo = JsonSerializer.Deserialize<GoogleUserInfo>(jsonResponse);
             return userInfo.ToBaseUserInfo();
         }

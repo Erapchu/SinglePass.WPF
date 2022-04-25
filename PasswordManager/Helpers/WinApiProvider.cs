8e4dbf071c17f5178fdc2a95867583abcfe07b76
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-04-25 10:14:24 +0700

    Update WinApiProvider.cs

--- PasswordManager/Helpers/WinApiProvider.cs
+++ PasswordManager/Helpers/WinApiProvider.cs
@@ -52,14 +52,29 @@ namespace PasswordManager.Helpers
         [DllImport("user32.dll")]
         public static extern bool ClientToScreen(IntPtr hWnd, ref POINT lpPoint);
 
+        public const int GWL_STYLE = -16;
         public const int GWL_EXSTYLE = -20;
-        public const int WS_EX_NOACTIVATE = 0x08000000;
+        public const uint WS_EX_NOACTIVATE = 0x08000000;
 
         [DllImport("user32.dll")]
-        public static extern IntPtr SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        public static extern uint GetWindowLong(IntPtr hWnd, int nIndex);
 
-        [DllImport("user32.dll")]
-        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        // This helper static method is required because the 32-bit version of user32.dll does not contain this API
+        // (on any versions of Windows), so linking the method will fail at run-time. The bridge dispatches the request
+        // to the correct function (GetWindowLong in 32-bit mode and GetWindowLongPtr in 64-bit mode)
+        public static IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong)
+        {
+            if (IntPtr.Size == 8)
+                return SetWindowLongPtr64(hWnd, nIndex, dwNewLong);
+            else
+                return new IntPtr(SetWindowLong32(hWnd, nIndex, dwNewLong.ToInt32()));
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+        private static extern int SetWindowLong32(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
+        private static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
 
         public const uint SWP_NOZORDER = 0x0004;
 

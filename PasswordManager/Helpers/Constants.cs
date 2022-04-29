50491db695bea1b47b5fa439fc0969bbe6bee28f
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-04-29 22:58:54 +0700

    Constants and logs => Purple

--- PasswordManager/Helpers/Constants.cs
+++ PasswordManager/Helpers/Constants.cs
@@ -5,20 +5,20 @@ namespace PasswordManager.Helpers
 {
     public static class Constants
     {
-        public const string AppName = "PasswordManager";
-        public const string PasswordsFileName = "PassMan.dat";
-        public const string CommonSettingsFileName = "CommonSettings.json";
-        public const string GoogleDriveFileName = "GoogleDrive.json";
+        public const string AppName = "Purple";
+        public const string PasswordsFileName = "purple.dat";
+        public const string CommonSettingsFileName = "commonSettings.json";
+        public const string GoogleDriveFileName = "googleDrive.json";
 
         public static string LocalAppDataDirectoryPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), AppName);
-        public static string RoamingAppDataDirectoryPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), AppName);
-        public static string PasswordsFilePath { get; } = Path.Combine(RoamingAppDataDirectoryPath, PasswordsFileName);
-        public static string CommonSettingsFilePath { get; } = Path.Combine(RoamingAppDataDirectoryPath, CommonSettingsFileName);
+        //public static string RoamingAppDataDirectoryPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), AppName);
+        public static string PasswordsFilePath { get; } = Path.Combine(LocalAppDataDirectoryPath, PasswordsFileName);
+        public static string CommonSettingsFilePath { get; } = Path.Combine(LocalAppDataDirectoryPath, CommonSettingsFileName);
         public static string GoogleDriveFilePath { get; } = Path.Combine(LocalAppDataDirectoryPath, GoogleDriveFileName);
 
         static Constants()
         {
-            CreateIfNotExists(RoamingAppDataDirectoryPath);
+            //CreateIfNotExists(RoamingAppDataDirectoryPath);
             CreateIfNotExists(LocalAppDataDirectoryPath);
         }
 

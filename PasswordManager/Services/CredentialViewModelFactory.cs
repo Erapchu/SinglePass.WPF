27e231eac4d0225bcc067cad75b57edcf0560488
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-08-07 19:17:06 +0700

    Optimized work with credential view models
    To avoid multiple loading from SQLite

--- PasswordManager/Services/CredentialViewModelFactory.cs
+++ PasswordManager/Services/CredentialViewModelFactory.cs
@@ -1,20 +1,35 @@
 ﻿using PasswordManager.Models;
 using PasswordManager.ViewModels;
+using System;
+using System.Collections.Concurrent;
 
 namespace PasswordManager.Services
 {
     public class CredentialViewModelFactory
     {
         private readonly FavIconCollector _favIconCollector;
+        private readonly ConcurrentDictionary<Guid, CredentialViewModel> _credentialVMs;
 
         public CredentialViewModelFactory(FavIconCollector favIconCollector)
         {
             _favIconCollector = favIconCollector;
+            _credentialVMs = new ConcurrentDictionary<Guid, CredentialViewModel>();
         }
 
         public CredentialViewModel ProvideNew(Credential credential)
         {
-            return new CredentialViewModel(credential, _favIconCollector);
+            if (_credentialVMs.TryGetValue(credential.Id, out CredentialViewModel res))
+                return res;
+
+            var newRes = new CredentialViewModel(credential, _favIconCollector);
+            _credentialVMs.TryAdd(newRes.Model.Id, newRes);
+
+            return newRes;
+        }
+
+        public bool RemoveCached(Guid id)
+        {
+            return _credentialVMs.TryRemove(id, out CredentialViewModel _);
         }
     }
 }

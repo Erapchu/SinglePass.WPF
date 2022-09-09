0f0ca09231e32e98a3a6a0a7624b245a299ab482
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-09-09 10:09:48 +0700

    Address bar extract shouldn't raise exception outside
    It should return string or null, but exceptions should be just logged

--- SinglePass.WPF/Services/AddressBarExtractor.cs
+++ SinglePass.WPF/Services/AddressBarExtractor.cs
@@ -1,4 +1,5 @@
-﻿using SinglePass.WPF.Helpers;
+﻿using Microsoft.Extensions.Logging;
+using SinglePass.WPF.Helpers;
 using System;
 using System.Collections.Concurrent;
 using System.Windows.Automation;
@@ -8,10 +9,11 @@ namespace SinglePass.WPF.Services
     public class AddressBarExtractor
     {
         private readonly ConcurrentDictionary<IntPtr, AutomationElement> _automationElementCache = new();
+        private readonly ILogger<AddressBarExtractor> _logger;
 
-        public AddressBarExtractor()
+        public AddressBarExtractor(ILogger<AddressBarExtractor> logger)
         {
-
+            _logger = logger;
         }
 
         // https://stackoverflow.com/questions/18897070/getting-the-current-tabs-url-from-google-chrome-using-c-sharp
@@ -22,35 +24,42 @@ namespace SinglePass.WPF.Services
         /// <returns>Address bar string</returns>
         public string ExtractAddressBar(IntPtr hwnd)
         {
-            for (int i = 0; i < 2; i++)
+            try
             {
-                try
+                for (int i = 0; i < 2; i++)
                 {
-                    if (!_automationElementCache.TryGetValue(hwnd, out AutomationElement addressBarElement))
+                    try
                     {
-                        AutomationElement element = AutomationElement.FromHandle(hwnd);
-                        if (element is null)
-                            return null;
+                        if (!_automationElementCache.TryGetValue(hwnd, out AutomationElement addressBarElement))
+                        {
+                            AutomationElement element = AutomationElement.FromHandle(hwnd);
+                            if (element is null)
+                                return null;
 
-                        _ = WinApiProvider.GetWindowThreadProcessId(hwnd, out uint processId);
-                        var processIdTrans = (int)processId;
+                            _ = WinApiProvider.GetWindowThreadProcessId(hwnd, out uint processId);
+                            var processIdTrans = (int)processId;
 
-                        Condition conditions = new AndCondition(
-                            new PropertyCondition(AutomationElement.ProcessIdProperty, processIdTrans),
-                            new PropertyCondition(AutomationElement.IsControlElementProperty, true),
-                            new PropertyCondition(AutomationElement.IsContentElementProperty, true),
-                            new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit));
+                            Condition conditions = new AndCondition(
+                                new PropertyCondition(AutomationElement.ProcessIdProperty, processIdTrans),
+                                new PropertyCondition(AutomationElement.IsControlElementProperty, true),
+                                new PropertyCondition(AutomationElement.IsContentElementProperty, true),
+                                new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit));
 
-                        addressBarElement = element.FindFirst(TreeScope.Descendants, conditions);
-                        _automationElementCache.TryAdd(hwnd, addressBarElement);
+                            addressBarElement = element.FindFirst(TreeScope.Descendants, conditions);
+                            _automationElementCache.TryAdd(hwnd, addressBarElement);
+                        }
+
+                        return ((ValuePattern)addressBarElement.GetCurrentPattern(ValuePattern.Pattern)).Current.Value;
                     }
-
-                    return ((ValuePattern)addressBarElement.GetCurrentPattern(ValuePattern.Pattern)).Current.Value;
+                    catch (ElementNotAvailableException)
+                    {
+                        _automationElementCache.TryRemove(hwnd, out _);
+                    }
                 }
-                catch (ElementNotAvailableException)
-                {
-                    _automationElementCache.TryRemove(hwnd, out _);
-                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, null);
             }
 
             return null;

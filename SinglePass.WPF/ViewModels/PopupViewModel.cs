f4ef730e9a9780b1c3d43a0b1b2c278934525210
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-09-09 09:48:25 +0700

    Implemented search password items in popup

--- SinglePass.WPF/ViewModels/PopupViewModel.cs
+++ SinglePass.WPF/ViewModels/PopupViewModel.cs
@@ -7,7 +7,9 @@ using SinglePass.WPF.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Unidecode.NET;
 
 namespace SinglePass.WPF.ViewModels
 {
@@ -28,6 +30,7 @@ namespace SinglePass.WPF.ViewModels
         private readonly ILogger<PopupViewModel> _logger;
         private readonly CredentialViewModelFactory _credentialViewModelFactory;
         private readonly AddressBarExtractor _addressBarExtractor;
+        private readonly List<CredentialViewModel> _credentialVMs = new();
 
         public event Action Accept;
 
@@ -38,6 +41,17 @@ namespace SinglePass.WPF.ViewModels
 
         [ObservableProperty]
         private bool _isLoading;
+
+        private string _searchText;
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                SetProperty(ref _searchText, value);
+                _ = DisplayCredentialsAsync();
+            }
+        }
 
         public IntPtr ForegroundHWND { get; set; }
 
@@ -100,7 +114,7 @@ namespace SinglePass.WPF.ViewModels
         [RelayCommand]
         private Task Loading()
         {
-            return Task.Run(() =>
+            return Task.Run(async () =>
             {
                 var tempCredentialsVM = new List<CredentialViewModel>();
                 try
@@ -124,6 +138,9 @@ namespace SinglePass.WPF.ViewModels
                             .ThenByDescending(c => c.LastModifiedTime)
                             .ToList();
                     }
+
+                    _credentialVMs.AddRange(tempCredentialsVM);
+                    await DisplayCredentialsAsync();
                 }
                 catch (Exception ex)
                 {
@@ -131,11 +148,68 @@ namespace SinglePass.WPF.ViewModels
                 }
                 finally
                 {
-                    DisplayedCredentials = new ObservableCollectionDelayed<CredentialViewModel>(tempCredentialsVM);
-                    OnPropertyChanged(nameof(DisplayedCredentials));
                     IsLoading = false;
                 }
             });
+        }
+
+        private async Task DisplayCredentialsAsync()
+        {
+            try
+            {
+                List<CredentialViewModel> filteredCredentials = null;
+                var filterText = SearchText;
+
+                if (string.IsNullOrEmpty(filterText))
+                {
+                    List<CredentialViewModel> tempList = null;
+                    await Task.Run(() =>
+                    {
+                        tempList = _credentialVMs.ToList();
+                    });
+                    filteredCredentials = tempList;
+                }
+                else
+                {
+                    filteredCredentials = await Task.Run(() =>
+                    {
+                        string transliteratedText = null;
+                        if (Regex.IsMatch(filterText, @"\p{IsCyrillic}"))
+                        {
+                            // there is at least one cyrillic character in the string
+                            transliteratedText = filterText.Unidecode();
+                        }
+                        var translitCompare = !string.IsNullOrWhiteSpace(transliteratedText);
+
+                        var fCreds = new List<CredentialViewModel>();
+                        foreach (var cred in _credentialVMs)
+                        {
+                            var nameValue = cred.NameFieldVM.Value;
+                            var loginValue = cred.LoginFieldVM.Value;
+                            var websiteValue = cred.SiteFieldVM.Value;
+                            var otherValue = cred.OtherFieldVM.Value;
+
+                            if (nameValue.IndexOf(filterText, StringComparison.OrdinalIgnoreCase) != -1
+                                || (loginValue != null && loginValue.IndexOf(filterText, StringComparison.OrdinalIgnoreCase) != -1)
+                                || (websiteValue != null && websiteValue.IndexOf(filterText, StringComparison.OrdinalIgnoreCase) != -1)
+                                || (otherValue != null && otherValue.IndexOf(filterText, StringComparison.OrdinalIgnoreCase) != -1)
+                                || (translitCompare && nameValue.IndexOf(transliteratedText, StringComparison.OrdinalIgnoreCase) != -1))
+                            {
+                                fCreds.Add(cred);
+                            }
+                        }
+
+                        return fCreds;
+                    });
+                }
+
+                DisplayedCredentials = new ObservableCollectionDelayed<CredentialViewModel>(filteredCredentials);
+                OnPropertyChanged(nameof(DisplayedCredentials));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, string.Empty);
+            }
         }
     }
 }

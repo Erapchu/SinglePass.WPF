e4fe44126653d041423bfb79e8589edd0e545dee
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-10-18 15:25:53 +0700

    Trying different logic on fetch fav icons

--- SinglePass.WPF/Services/FavIconCollector.cs
+++ SinglePass.WPF/Services/FavIconCollector.cs
@@ -20,15 +20,15 @@ namespace SinglePass.WPF.Services
 
     public class FavIconCollector : IFavIconCollector
     {
-        private const int _processingTimeout = 100;
+        //private const int _processingTimeout = 100;
         private const string _favIconServiceUrl = "http://www.google.com/s2/favicons?domain_url={0}&sz={1}";
 
-        private readonly RegeneratedList<ProcessingImageWrapper> _processingImages = new();
         private readonly ILogger<FavIconCollector> _logger;
         private readonly ImageService _imageService;
-        private readonly IServiceScopeFactory _serviceScopeFactory;
-        private readonly CancellationTokenSource _processingCTS = new();
+        //private readonly IServiceScopeFactory _serviceScopeFactory;
+        //private readonly CancellationTokenSource _processingCTS = new();
         private readonly ConcurrentDictionary<ProcessingImageWrapper, ImageSource> _imagesCache = new();
+        private readonly ConcurrentDictionary<ProcessingImageWrapper, Task<ImageSource>> _processingImages = new();
 
         public FavIconCollector(
             ILogger<FavIconCollector> logger,
@@ -36,10 +36,10 @@ namespace SinglePass.WPF.Services
             IServiceScopeFactory serviceScopeFactory)
         {
             _imageService = imageService;
-            _serviceScopeFactory = serviceScopeFactory;
+            //_serviceScopeFactory = serviceScopeFactory;
             _logger = logger;
 
-            _ = Task.Run(() => ImageProcessing(_processingCTS.Token));
+            //_ = Task.Run(() => ImageProcessing(_processingCTS.Token));
         }
 
         public void ScheduleGetImage(string imageUrlString, Action<ImageSource> setPropertyAction, int size = 16)
@@ -51,95 +51,112 @@ namespace SinglePass.WPF.Services
 
                 var processingImageWrapper = new ProcessingImageWrapper(imageUrlString, setPropertyAction, size);
 
-                if (_imagesCache.TryGetValue(processingImageWrapper, out ImageSource image))
+                if (_imagesCache.TryGetValue(processingImageWrapper, out ImageSource cachedImage))
                 {
-                    processingImageWrapper.SetPropertyAction.Invoke(image);
+                    processingImageWrapper.SetPropertyAction.Invoke(cachedImage);
                 }
                 else
                 {
-                    _processingImages.Add(processingImageWrapper);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, null);
-            }
-        }
-
-        private async Task ImageProcessing(CancellationToken cancellationToken)
-        {
-            try
-            {
-                using var scope = _serviceScopeFactory.CreateScope();
-                var favIconCacheService = scope.ServiceProvider.GetService<FavIconCacheService>();
-                await favIconCacheService.Migrate();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, null);
-            }
-
-            while (true)
-            {
-                try
-                {
-                    cancellationToken.ThrowIfCancellationRequested();
-
-                    var processingWrappers = _processingImages.PopAll();
-                    if (processingWrappers != null)
+                    if (_processingImages.TryGetValue(processingImageWrapper, out Task<ImageSource> task))
                     {
-                        using var scope = _serviceScopeFactory.CreateScope();
-                        var favIconCacheService = scope.ServiceProvider.GetService<FavIconCacheService>();
-                        var uniqueHosts = processingWrappers
-                            .Distinct()
-                            .Select(ipw => new FavIconDto() { Host = ipw.Host, Size = ipw.Size })
-                            .ToList();
-                        var favIconsFromDB = await favIconCacheService.GetMany(uniqueHosts);
-                        var tempFavIconCacheFromDB = favIconsFromDB.ToDictionary(fi => new ProcessingImageWrapper(fi.Host, fi.Size));
-
-                        // Set existing to UI
-                        foreach (var processingWrapper in processingWrappers)
-                        {
-                            ImageSource cachedImageSource;
-                            if (tempFavIconCacheFromDB.TryGetValue(processingWrapper, out FavIcon favIcon))
-                            {
-                                var imageSource = ImageSourceHelper.ToImageSource(favIcon.Bytes);
-                                processingWrapper.SetPropertyAction.Invoke(imageSource);
-                                cachedImageSource = imageSource;
-                            }
-                            else
-                            {
-                                // Download, set to DB, set to UI and save to temp local cache
-                                var bitmapImage = await _imageService.GetImageAsync(
-                                    string.Format(_favIconServiceUrl, processingWrapper.Host, processingWrapper.Size), cancellationToken);
-                                var freshFavIcon = new FavIcon()
-                                {
-                                    Bytes = ImageSourceHelper.ToBytes(bitmapImage),
-                                    Host = processingWrapper.Host,
-                                    Size = processingWrapper.Size,
-                                };
-                                await favIconCacheService.Add(freshFavIcon);
-                                tempFavIconCacheFromDB.TryAdd(processingWrapper, freshFavIcon);
-                                processingWrapper.SetPropertyAction.Invoke(bitmapImage);
-                                cachedImageSource = bitmapImage;
-                            }
-
-                            _imagesCache.TryAdd(processingWrapper, cachedImageSource);
-                        }
+                        task.ContinueWith(t => processingImageWrapper.SetPropertyAction.Invoke(task.Result));
                     }
-
-                    await Task.Delay(_processingTimeout, cancellationToken);
-                }
-                catch (OperationCanceledException)
-                {
-                    break;
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, null);
+                    else
+                    {
+                        var t = Task.Run<ImageSource>(async () =>
+                        {
+                            var imageSource = await _imageService.GetImageAsync(
+                                string.Format(_favIconServiceUrl, processingImageWrapper.Host, processingImageWrapper.Size),
+                                CancellationToken.None);
+                            processingImageWrapper.SetPropertyAction(imageSource);
+                            _imagesCache.TryAdd(processingImageWrapper, imageSource);
+                            _processingImages.TryRemove(processingImageWrapper, out _);
+                            return imageSource;
+                        });
+                        _processingImages.TryAdd(processingImageWrapper, t);
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, null);
+            }
         }
+
+        //private async Task ImageProcessing(CancellationToken cancellationToken)
+        //{
+        //    try
+        //    {
+        //        using var scope = _serviceScopeFactory.CreateScope();
+        //        var favIconCacheService = scope.ServiceProvider.GetService<FavIconCacheService>();
+        //        await favIconCacheService.Migrate();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError(ex, null);
+        //    }
+
+        //    while (true)
+        //    {
+        //        try
+        //        {
+        //            cancellationToken.ThrowIfCancellationRequested();
+
+        //            var processingWrappers = _processingImages.PopAll();
+        //            if (processingWrappers != null)
+        //            {
+        //                using var scope = _serviceScopeFactory.CreateScope();
+        //                var favIconCacheService = scope.ServiceProvider.GetService<FavIconCacheService>();
+        //                var uniqueHosts = processingWrappers
+        //                    .Distinct()
+        //                    .Select(ipw => new FavIconDto() { Host = ipw.Host, Size = ipw.Size })
+        //                    .ToList();
+        //                var favIconsFromDB = await favIconCacheService.GetMany(uniqueHosts);
+        //                var tempFavIconCacheFromDB = favIconsFromDB.ToDictionary(fi => new ProcessingImageWrapper(fi.Host, fi.Size));
+
+        //                // Set existing to UI
+        //                foreach (var processingWrapper in processingWrappers)
+        //                {
+        //                    ImageSource cachedImageSource;
+        //                    if (tempFavIconCacheFromDB.TryGetValue(processingWrapper, out FavIcon favIcon))
+        //                    {
+        //                        var imageSource = ImageSourceHelper.ToImageSource(favIcon.Bytes);
+        //                        processingWrapper.SetPropertyAction.Invoke(imageSource);
+        //                        cachedImageSource = imageSource;
+        //                    }
+        //                    else
+        //                    {
+        //                        // Download, set to DB, set to UI and save to temp local cache
+        //                        var bitmapImage = await _imageService.GetImageAsync(
+        //                            string.Format(_favIconServiceUrl, processingWrapper.Host, processingWrapper.Size), cancellationToken);
+        //                        var freshFavIcon = new FavIcon()
+        //                        {
+        //                            Bytes = ImageSourceHelper.ToBytes(bitmapImage),
+        //                            Host = processingWrapper.Host,
+        //                            Size = processingWrapper.Size,
+        //                        };
+        //                        await favIconCacheService.Add(freshFavIcon);
+        //                        tempFavIconCacheFromDB.TryAdd(processingWrapper, freshFavIcon);
+        //                        processingWrapper.SetPropertyAction.Invoke(bitmapImage);
+        //                        cachedImageSource = bitmapImage;
+        //                    }
+
+        //                    _imagesCache.TryAdd(processingWrapper, cachedImageSource);
+        //                }
+        //            }
+
+        //            await Task.Delay(_processingTimeout, cancellationToken);
+        //        }
+        //        catch (OperationCanceledException)
+        //        {
+        //            break;
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            _logger.LogError(ex, null);
+        //        }
+        //    }
+        //}
 
         [DebuggerDisplay("{Size} - {_originalString}")]
         private class ProcessingImageWrapper
